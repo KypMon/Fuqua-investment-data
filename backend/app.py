8ed1f7b3bb62254178a3af6a50207b9591573307
Linda Cole <lcc9@duke.edu>
2025-11-22 13:30:56 -0500

    commit

--- backend/app.py
+++ backend/app.py
@@ -8,23 +8,15 @@ from src.services.file_service import FileService
 from src.middleware.authentication import Authentication
 from src.middleware.before_request_hook import BeforeRequestHook
 from src.routes.api_routes import ApiRoutes
-import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--log", type=str)
-parser.add_argument("--config", type=str)
-
-args = parser.parse_args()
-log = AppLogger.set_up_logger(args.log)
-config = Config.set_up_config(args.config)
-
+"""
+Application factory: used both by Flask (dev) and Gunicorn (prod)
+"""
 def create_app(config_file=None, log_file=None):
-    """
-    Application factory: used both by Flask (dev) and Gunicorn (prod)
-    """
     config_file = config_file or os.environ.get("APP_CONFIG_FILE", "config/.env")
     log_file = log_file or os.environ.get("APP_LOG_FILE", "app.log")
 
+    AppLogger.set_up_logger(log_file)
     Config.set_up_config(config_file)
 
     FileService().make_static_dir()
@@ -46,19 +38,29 @@ def create_app(config_file=None, log_file=None):
 
     return app
 
+# if __name__ == "__main__": 
+#     # for local development ("python app.py ....")
+#     app = create_app()
+
+#     app.run(
+#         host=Config.get_property("HOST"),
+#         port=Config.get_property("PORT"),
+#         debug=False
+#     )
+# else:  # For gunicorn server threads 
+#     print("FOR Gunicorn?")
+#     app = create_app()
+
+app = create_app()
 if __name__ == "__main__": 
+    # have Flask serve resources
     # for local development ("python app.py ....")
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--config", dest="config_file", default="config/.env")
-    parser.add_argument("--log", dest="log_file", default="app.log")
-    args = parser.parse_args()
-
-    app = create_app(config_file=args.config_file, log_file=args.log_file)
-
     app.run(
         host=Config.get_property("HOST"),
         port=Config.get_property("PORT"),
         debug=False
     )
-else:  # For gunicorn server threads 
-    app = create_app()
+else:
+    # gunicorn is the server, no flask
+    pass
+
