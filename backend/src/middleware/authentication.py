c4145fda55bb6dfe278896bdb4f4d108139c1e41
Linda Cole <lcc9@duke.edu>
2025-12-02 11:29:58 -0500

    commit

--- backend/src/middleware/authentication.py
+++ backend/src/middleware/authentication.py
@@ -54,41 +54,7 @@ class Authentication(object):
             environ["status_code"] = 200
             return self.app(environ, start_response)
         
-        # NO!  apparently this was causing an error I don't need to deal with
-        # begin debug
-        # response = requests.get(Authentication.auth_config["jwks_uri"])
-        # response.raise_for_status()  # Raise an exception for HTTP errors
-
-        # jwks = response.json()  # Parse the JWKS as JSON
-        # print(json.dumps(jwks, indent=2))  # Pretty-print the keys
-
-        # # Optional: inspect key IDs ("kid")
-        # if "keys" in jwks:
-        #     key_ids = [key.get("kid") for key in jwks["keys"]]
-        #     print("Available key IDs:", key_ids)
-        # else:
-        #     print("No 'keys' field found in JWKS response!")
-        # end debug
-
-        # https://flask.palletsprojects.com/en/3.0.x/api/#flask.Request.path
-        #self.logger.info("request.base_url " + str(request.base_url))
-        #self.logger.info("request.path " + str(request.path))
-        #self.logger.info("request.headers " + str(request.headers))
-        #self.logger.info(str(request.headers.get("Cookie")))
-        #self.logger.info("request.authorization " + str(request.authorization))
-        #self.logger.info("request.host_url " + request.host_url)
-        #self.logger.info("request.cookies -> " + str(request.cookies))
-        #self.logger.info("request.args -> " + str(request.args))
-        #self.logger.info("request.application -> " + str(request.application))
-        #self.logger.info("request.data -> " + str(request.data))
-        #self.logger.info("request.full_path -> " + str(request.full_path))
-
-        # for key,value in environ.items():
-        #     if key == "HTTP_COOKIE" or key == "HTTP_HOST" or key == "REQUEST_URI":
-        #         self.logger.info(key + " -> " + str(value))
-        
-        #self.logger.info(f"Request path: {request.path}, full_path: {request.full_path}")
-        #self.logger.info(f"Cookie header: {request.headers.get('Cookie')}")
+        self.debug_statements(request, environ)
 
         try:
             data = request.headers.get("Cookie") if request.headers.get("Cookie") is not None \
@@ -105,27 +71,8 @@ class Authentication(object):
                 environ["status_code"] = 401
                 return self.app(environ, start_response)
             
-            # begin debug
-            header = jwt.get_unverified_header(JWT)
-            self.logger.info("JWT header kid: " + str(header.get("kid")))
-
-            claims_unverified = jwt.decode(JWT, options={"verify_signature": False})
-            self.logger.info(f"Unverified claims: {claims_unverified}")
-            # end debug
-
             # https://pyjwt.readthedocs.io/en/stable/usage.html#retrieve-rsa-signing-keys-from-a-jwks-endpoint
             signing_key = self.extract_signing_key(JWT) 
-
-            # claims = jwt.decode(
-            #     JWT,
-            #     key=signing_key.key,
-            #     algorithms=Authentication.auth_config["algorithm"], # RS256
-            #     options={"verify_exp": True, "verify_iat": False}, # expiration time, issued at time
-            #     audience=Authentication.auth_config["audience"], #"FuquaWorld",
-            #     issuer=Authentication.auth_config["issuer"] # https://go-dev.fuqua.duke.edu/auth
-            # )
-            #self.logger.info("=== STARTING JWT DECODE ===")
-            #self.logger.info(f"Using alg: {Authentication.auth_config['algorithm']}")
 
             claims = jwt.decode(
                 JWT,
@@ -136,7 +83,6 @@ class Authentication(object):
                 audience=Authentication.auth_config["audience"],
                 issuer=Authentication.auth_config["issuer"],
             )
-            #self.logger.info(str(request.full_path) + " DECODE SUCCESSFUL")
 
             if claims is None: # return 401
                 self.logger.info(str(request.full_path) + " No claims taken from JWT ... returning 401")
@@ -189,3 +135,24 @@ class Authentication(object):
         jwks_client = PyJWKClient(Authentication.auth_config["jwks_uri"] )
         signing_key = jwks_client.get_signing_key_from_jwt(JWT)
         return signing_key
+    
+    def debug_statements(self, request, environ):
+        # https://flask.palletsprojects.com/en/3.0.x/api/#flask.Request.path
+        self.logger.info("request.base_url " + str(request.base_url))
+        self.logger.info("request.path " + str(request.path))
+        self.logger.info("request.headers " + str(request.headers))
+        self.logger.info(str(request.headers.get("Cookie")))
+        self.logger.info("request.authorization " + str(request.authorization))
+        self.logger.info("request.host_url " + request.host_url)
+        self.logger.info("request.cookies -> " + str(request.cookies))
+        self.logger.info("request.args -> " + str(request.args))
+        self.logger.info("request.application -> " + str(request.application))
+        self.logger.info("request.data -> " + str(request.data))
+        self.logger.info("request.full_path -> " + str(request.full_path))
+
+        for key,value in environ.items():
+            if key == "HTTP_COOKIE" or key == "HTTP_HOST" or key == "REQUEST_URI":
+                self.logger.info(key + " -> " + str(value))
+        
+        self.logger.info(f"Request path: {request.path}, full_path: {request.full_path}")
+        self.logger.info(f"Cookie header: {request.headers.get('Cookie')}")
